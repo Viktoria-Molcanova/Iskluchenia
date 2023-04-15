b7d2fd1b6c5a8d96e1b38e33b5d704d8232411e9
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2023-04-15 18:09:23 +0700

    create

---
+++ 2 seminar/2.3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+  
+     //Метод создания строки и запроса данных
+  
+    public static void main(String[] args) {
+      String str = in.nextLine();
+      
+      //Выброс исключения пустые строки
+      
+    if(str.isEmpty())
+      throw new IllegalArgumentException("Нельзя вводить пустые строки!");
+      System.out.println("Повторите попытку ввода.");
+      scanner.nextLine();
+    else
+      System.out.println(str);
+    return str;
+  }
+}
