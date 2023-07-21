ec4c4d5e05269cc9efeea65099f346d4dc65a5b1
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2023-07-21 23:27:32 +0700

    Update 1.java

--- 3 seminar/1.java
+++ 3 seminar/1.java
@@ -3,7 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class PY {
+public class JV {
      public static void main(String[] args) {
 
           class INPUT {
